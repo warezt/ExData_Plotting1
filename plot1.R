e972d82b5feb45b1834238e7f5436141e8eb9389
warezt <tanakorn.dhamalongkrot@gmail.com>
2017-02-25 16:44:21 +0700

    Fixes download method

--- plot1.R
+++ plot1.R
@@ -1,8 +1,9 @@
-'Section1 Get file from web
+###Section1 Get file from web
 
 linkfromweb<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 zippedfile<-"./data/household_power_consumption.zip"
 extractedfile<-"./data/household_power_consumption.txt"
 
-if(!file.exists(zippedfile)){download.file(linkfromweb,zippedfile,method="curl")
-  upzip(zippedfile,overwrite=T,exdir="./data")}
+if(!file.exists(extractedfile)){
+  download.file(linkfromweb,zippedfile)##No need for method="curl" as it is window
+  unzip(zippedfile,overwrite=T,exdir="./data")}
