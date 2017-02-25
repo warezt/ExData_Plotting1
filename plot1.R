994de4ad39df4d807e1fd396b44e94affb4361fe
warezt <tanakorn.dhamalongkrot@gmail.com>
2017-02-25 18:21:32 +0700

    Get POSIXlt vector

--- plot1.R
+++ plot1.R
@@ -11,5 +11,13 @@ if(!file.exists(extractedfile)){
 ##Section2 Get data and strip out between 1-2 Feb 2007
 rawdata<-read.table(extractedfile,header=T,sep=";",na.strings="?")
 #Matching doc https://stat.ethz.ch/R-manual/R-devel/library/base/html/match.html
+#Get the data for 2 days
+selecteddata <- rawdata[rawdata$Date %in% c("1/2/2007","2/2/2007"),]
+#Test if data have 2 days
+if(length(unique(selecteddata$Date))!=2){"Wrong"}
+#Create character vector
+date_and_time<-paste(selecteddata$Date, selecteddata$Time, sep=" ")
+#Convert character vector to POSIXlt vector
+date_and_time_as_POSIXlt<-strptime(date_and_time,"%d/%m/%Y %H:%M:%S")
 
 
