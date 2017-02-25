b15cb81c8e9c0a5a685c00ce3fa010850801200d
warezt <tanakorn.dhamalongkrot@gmail.com>
2017-02-25 17:11:44 +0700

    Add matching doc

--- plot1.R
+++ plot1.R
@@ -7,3 +7,9 @@ extractedfile<-"./data/household_power_consumption.txt"
 if(!file.exists(extractedfile)){
   download.file(linkfromweb,zippedfile)##No need for method="curl" as it is window
   unzip(zippedfile,overwrite=T,exdir="./data")}
+
+##Section2 Get data and strip out between 1-2 Feb 2007
+rawdata<-read.table(extractedfile,header=T,sep=";",na.strings="?")
+#Matching doc https://stat.ethz.ch/R-manual/R-devel/library/base/html/match.html
+
+
