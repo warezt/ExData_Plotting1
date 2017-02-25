9825099e1ff8078b3762c96d0326118b58d32641
warezt <tanakorn.dhamalongkrot@gmail.com>
2017-02-25 23:00:21 +0700

    Finished 4 plot

--- plot3.R
+++ plot3.R
@@ -29,7 +29,7 @@ finaldata<-cbind(date_and_time_as_POSIXlt,selecteddata)
 ##qplot(finaldata$Global_active_power)
 
 ##Section 3; Create plot
-hist(finaldata$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
-
-png(filename = "plot1.png",width = 480, height = 480, units = "px",bg = "transparent")
-dev.off()
+plot(finaldata$date_and_time_as_POSIXlt, finaldata$Sub_metering_1, type="l", col="black", xlab="", ylab="Energy sub metering")
+lines(finaldata$date_and_time_as_POSIXlt, finaldata$Sub_metering_2, col="red")
+lines(finaldata$date_and_time_as_POSIXlt, finaldata$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty="solid")
